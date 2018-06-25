04e039c298150e7b5a51ecd168771ee46b6adbc7
itssooofluffy <s0546860@htw-berlin.de>
2018-06-25 18:24:12 +0200

    Midi

---
+++ js/audio-manager.js
@@ -0,0 +1,15 @@
+class AudioManager {
+  constructor() {
+    this.ctx = null;
+    try {
+      // Fix up for prefixing
+      window.AudioContext = window.AudioContext||window.webkitAudioContext;
+      this.ctx = new AudioContext();
+    } catch(e) {
+      console.error(e);
+      alert('Web Audio API is not supported in this browser');
+    }
+  }
+}
+
+export default new AudioManager();
